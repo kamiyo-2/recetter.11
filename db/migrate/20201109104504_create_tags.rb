5bbf6e1425ee3fe232719884d56adc066a513c0c
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-11-09 19:48:04 +0900

    recipe_tag_relationモデル作成

--- db/migrate/20201109104504_create_tags.rb
+++ db/migrate/20201109104504_create_tags.rb
@@ -2,6 +2,8 @@ class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
       t.string :name, null:false
+      t.references :recipe, foreign_key: true
+      t.references :tag, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_104504) do
+ActiveRecord::Schema.define(version: 2020_11_09_104708) do
+
+  create_table "recipe_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
