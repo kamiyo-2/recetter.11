8a5d02f3ef894f600c98a148ccd7013d22c90ffc
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-11-10 05:14:56 +0900

    tags.index.html.erb作成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,18 @@
-class TagsController < ApplicationController
+class TextsController < ApplicationController
+  
+  def create
+    @text = Text.new(text_params)
+    if @text.save
+      redirect_to recipe_path(@text.recipe)
+    else
+      @recipe = @text.recipe
+      @texts = @recipe.texts
+      render "recipes/show"
+  end
+  end
 
-  def index
-    @tags = Tag.all
+  private
+  def text_params
+    params.require(:text).permit(:text, :text_image).merge(recipe_id: params[:recipe_id])
   end
 end
